2903ea73de1d5b3a73746837624c43f15bd6f069
Digvijay <digvijag@thoughtworks.com>
2014-01-21 10:39:34 +0530

    evaluator handles addition and Subtraction.

--- src/digvijag/evaluator/lib/ExpressionEvaluatorLibrary.java
+++ src/digvijag/evaluator/lib/ExpressionEvaluatorLibrary.java
@@ -1,5 +1,23 @@
 package digvijag.evaluator.lib;
 
 public class ExpressionEvaluatorLibrary {
+    String expression = "";
+    int numberOne, numberTwo, result;
 
+    public int evaluate(String expression) {
+        this.expression = expression;
+        for (int i = 0; i < expression.length(); i++) {
+            if ('+' == expression.charAt(i)) {
+                numberOne = Integer.parseInt(expression.substring(0, i));
+                numberTwo = Integer.parseInt(expression.substring(i, expression.length()));
+                result = numberOne + numberTwo;
+            }
+            if ('-' == expression.charAt(i)) {
+                numberOne = Integer.parseInt(expression.substring(0, i));
+                numberTwo = Integer.parseInt(expression.substring(i, expression.length()));
+                result = numberOne + numberTwo;
+            }
+        }
+        return result;
+    }
 }
--- test/digvijag/lib/ExpressionEvaluatorLibraryTest.java
+++ test/digvijag/lib/ExpressionEvaluatorLibraryTest.java
@@ -1,12 +1,23 @@
 package digvijag.lib;
 
+import digvijag.evaluator.lib.ExpressionEvaluatorLibrary;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
 public class ExpressionEvaluatorLibraryTest {
     @Test
-    public void main() {
-        assertEquals(true, true);
+    public void test_addition_of_two_integers() {
+        ExpressionEvaluatorLibrary evaluator = new ExpressionEvaluatorLibrary();
+        int expected = 5;
+        int actual = evaluator.evaluate("2+3");
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void test_subtraction_of_two_integers() {
+        ExpressionEvaluatorLibrary evaluator = new ExpressionEvaluatorLibrary();
+        int expected = 1;
+        int actual = evaluator.evaluate("3-2");
+        assertEquals(expected,actual);
     }
 }
