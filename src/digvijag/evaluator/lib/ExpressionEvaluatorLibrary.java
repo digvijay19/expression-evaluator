911485b52506d68524160a1ecd5924315b60a654
Digvijay <digvijag@thoughtworks.com>
2014-01-23 12:35:02 +0530

    evaluates with simple brackets.

--- src/digvijag/evaluator/lib/ExpressionEvaluatorLibrary.java
+++ src/digvijag/evaluator/lib/ExpressionEvaluatorLibrary.java
@@ -7,26 +7,47 @@ public class ExpressionEvaluatorLibrary {
         String singleExpression;
         SingleOperationEvaluator singleOperation = new SingleOperationEvaluator();
         String availableOperations = "+-*/^";
-        int resultOfSingleOperation;
-        int check = 0;
-
+        int resultOfSingleOperation, resetValue = 0;
+        int operationCheck = 0;
+        if (modifiedExpression.contains("(")) {
+            modifiedExpression = solveBrackets(expression);
+        }
         for (int i = 0; i < modifiedExpression.length(); i++) {
             String currentCharacter = Character.toString(modifiedExpression.charAt(i));
             if (availableOperations.contains(currentCharacter)) {
-                check++;
-                if (check == 2) {
+                operationCheck++;
+                if (operationCheck == 2) {
                     singleExpression = modifiedExpression.substring(0, i);
                     resultOfSingleOperation = singleOperation.evaluateOperation(singleExpression);
                     modifiedExpression = getModifiedExpression(resultOfSingleOperation, modifiedExpression, i);
-                    check = 0;
-                    i = 0;
+                    operationCheck = resetValue;
+                    i = resetValue;
                 }
             }
         }
         return singleOperation.evaluateOperation(modifiedExpression);
     }
 
-    private String getModifiedExpression(Integer numberToConcat, String tempExpression, int position) {
+    private String solveBrackets(String expression) {
+        String newExpression, expressionInBracket;
+        int indexOfOpeningBracket = 0;
+        int indexOfClosingBracket = 0;
+        int resultOfBracket;
+        for (int index = 0; index < expression.length(); index++) {
+            if ('(' == expression.charAt(index)) {
+                indexOfOpeningBracket = index;
+            }
+            if (')' == expression.charAt(index)) {
+                indexOfClosingBracket = index;
+            }
+        }
+        expressionInBracket = expression.substring(indexOfOpeningBracket + 1, indexOfClosingBracket);
+        resultOfBracket = this.evaluate(expressionInBracket);
+        newExpression = expression.replace('(' + expressionInBracket + ')', Integer.toString(resultOfBracket));
+        return newExpression;
+    }
+
+    private String getModifiedExpression(int numberToConcat, String tempExpression, int position) {
         String one = Integer.toString(numberToConcat);
         String two = tempExpression.substring(position, tempExpression.length());
         return one + two;
---
+++ test/digvijag/lib/BracketsTest.java
@@ -0,0 +1,16 @@
+package digvijag.lib;
+
+import digvijag.evaluator.lib.ExpressionEvaluatorLibrary;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class BracketsTest {
+    @Test
+    public void test_solve_expression_with_single_bracket() {
+        ExpressionEvaluatorLibrary evaluator = new ExpressionEvaluatorLibrary();
+        int expected = 5;
+        int actual = evaluator.evaluate("(3+2)");
+        assertEquals(expected, actual);
+    }
+}
