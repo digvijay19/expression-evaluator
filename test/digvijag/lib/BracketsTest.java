5356d55510a54cfa640d9a33b6a20158094a1961
Digvijay <digvijag@thoughtworks.com>
2014-01-23 12:36:41 +0530

    evaluates with multiple non-nested brackets.

--- test/digvijag/lib/BracketsTest.java
+++ test/digvijag/lib/BracketsTest.java
@@ -13,4 +13,12 @@ public class BracketsTest {
         int actual = evaluator.evaluate("(3+2)");
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void test_solve_expression_with_two_bracket() {
+        ExpressionEvaluatorLibrary evaluator = new ExpressionEvaluatorLibrary();
+        int expected = 10;
+        int actual = evaluator.evaluate("(3+2)+(3+2)");
+        assertEquals(expected, actual);
+    }
 }
